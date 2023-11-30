831dfa35945bfabe9c0e4d29738f5f5b1ca49f00
Ian Miller <imiller142@gmail.com>
2023-11-30 17:41:56 -0600

    added class file and main file

---
+++ Account.py
@@ -0,0 +1,12 @@
+﻿class Account:
+#used to initialize account can be used as template for variations in accounts.
+    def __init__(self, name, value):
+        #initialize the account with a starting value
+        self.name = name
+        self.value = value
+
+    def add_money(self, value):
+        #function to add money to the account
+        self.value += value
+        return print("{} account now contains {}".format(self.name, self.value))
+
---
+++ main.py
@@ -0,0 +1,5 @@
+﻿from Account import Account as a
+
+banking = a('Savings', 1200)
+
+banking.add_money(250)
